efd3faf99eea63fed87295f9616a185719445d6f
Martez Knox <martez@bakedbot.ai>
2025-11-05 20:34:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-567050101-bc6e8",
+  "appId": "1:1016399212569:web:d9c43842ea4d824e13ba88",
+  "apiKey": "AIzaSyASUULiUcdtqVnPrTqZTsxoNiXdFPJ5e7E",
+  "authDomain": "studio-567050101-bc6e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1016399212569"
+};
