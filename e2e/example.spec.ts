ad28886b7c3766e97fbe903216f29f64b0804665
Martez Knox <martez@bakedbot.ai>
2025-11-07 15:02:02 +0000

    Lets continue preparing for production

---
+++ e2e/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title to contain a substring.
+  await expect(page).toHaveTitle(/BakedBot AI Assistant/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('/');
+
+  // Find the h1 element with the text "Find Your Bliss"
+  const heading = page.locator('h1', { hasText: 'Find Your Bliss' });
+
+  // Expect the h1 element to be visible.
+  await expect(heading).toBeVisible();
+});
