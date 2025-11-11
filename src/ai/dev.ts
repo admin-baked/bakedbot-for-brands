a07bcb8e67c5d33c991e6572cde3161663f72afd
Martez Knox <martez@bakedbot.ai>
2025-11-11 02:13:25 +0000

    New Key: SG.kWF-wQfJSxOeYS8IzEPEXg.PsGwxFm95B5O2eZGBSQjoQnYfef_BUB1QWu_H

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -9,4 +9,3 @@ import './tools/get-product-reviews.ts';
 import './tools/get-product.ts';
 import './flows/summarize-reviews.ts';
 import './ai-powered-product-recommendations.ts';
-import './flows/send-order-email.ts';
