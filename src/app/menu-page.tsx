837c088f9a31abba191894f23a77599baddaa134
Martez Knox <martez@bakedbot.ai>
2025-11-12 22:23:00 +0000

    Looks the same

--- src/app/menu-page.tsx
+++ src/app/menu-page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import Header from '@/app/components/header';
 import { HeroSlider } from '@/components/hero-slider';
 import { DispensaryLocator } from '@/components/dispensary-locator';
