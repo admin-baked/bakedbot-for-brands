905513d5e7150ce46df5235d7c1e93e4c8872f85
Martez Knox <martez@bakedbot.ai>
2025-11-13 04:16:36 +0000

    Desktop login works but on mobile login for all roles we see this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-567050101-bc6e8",
   "appId": "1:1016399212569:web:d9c43842ea4d824e13ba88",
   "apiKey": "AIzaSyASUULiUcdtqVnPrTqZTsxoNiXdFPJ5e7E",
-  "authDomain": "studio-567050101-bc6e8.firebaseapp.com",
+  "authDomain": "brands.bakedbot.ai",
   "measurementId": "G-B4FT9QTWD1",
   "messagingSenderId": "1016399212569"
 };
