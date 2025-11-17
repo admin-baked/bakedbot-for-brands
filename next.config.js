a7e1157cfa5f4832e35fc2d2139d14d2a29db8d9
Martez Knox <martez@bakedbot.ai>
2025-11-17 08:15:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -36,4 +32,4 @@ const nextConfig = {
   }
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
