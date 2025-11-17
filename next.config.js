7dccd25b026cbdc7219738981fe9568e4ee5f9b9
Martez Knox <martez@bakedbot.ai>
2025-11-17 10:14:36 +0000

    Cool, the 404 is actually progress.
    Before, `/` was rendering a `Home`

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'studio-567050101-bc6e8.firebasestorage.app',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
-  },
-  experimental: {
-    optimizePackageImports: ['@radix-ui/react-dropdown-menu', '@radix-ui/react-dialog']
-  }
 };
 
 module.exports = nextConfig;
