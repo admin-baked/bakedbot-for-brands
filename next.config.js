c6367468546b9be1349d5bbdac9c0a96303a1953
Martez Knox <martez@bakedbot.ai>
2025-11-18 00:48:19 +0000

    Nice, root page dragon defeated 🐉✅
    Now this one’s simple: Next/Image i

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
   },
 };
 
