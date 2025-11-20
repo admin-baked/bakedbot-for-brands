d124e5a7159ae1d2b28adecf351bdf48ca2dd013
Martez Knox <martez@bakedbot.ai>
2025-11-20 06:45:34 +0000

    Nice, we’re down to five very specific, very fixable complaints. Let’s s

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,3 +3,5 @@
 // Direct export of default assets to prevent broken links.
 // These URLs point to publicly accessible files in a Google Cloud Storage bucket.
 export const defaultLogo = 'https://minoritycannabis.org/wp-content/uploads/2025/03/att.yne0IiSw3BIPejWlvseVXNlEGfA5E9CjF7HO2ecLTGQ.jpeg';
+
+export const defaultChatbotIcon = 'https://storage.googleapis.com/stedi-assets/misc/smokey-icon-1.png';
