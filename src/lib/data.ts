f77296d02dc6271f97c4f44096f00b026d36057d
Rishabh Suravaram <rishabh@bakedbot.ai>
2025-11-21 14:49:59 +0000

    Lets use this image as the chatbot widget: https://bakedbot.ai/wp-conten

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,5 @@
-
-
 // Direct export of default assets to prevent broken links.
 // These URLs point to publicly accessible files in a Google Cloud Storage bucket.
 export const defaultLogo = 'https://minoritycannabis.org/wp-content/uploads/2025/03/att.yne0IiSw3BIPejWlvseVXNlEGfA5E9CjF7HO2ecLTGQ.jpeg';
 
-export const defaultChatbotIcon = 'https://bakedbot.ai/wp-content/uploads/2025/11/founding-citrcle.png';
+export const defaultChatbotIcon = 'https://bakedbot.ai/wp-content/uploads/2025/11/SMokey-Chat-scaled.png';
