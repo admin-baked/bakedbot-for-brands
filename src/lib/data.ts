76d9bb18f1d169e16d8995873554b1545d3c734a
Martez Knox <martez@bakedbot.ai>
2025-11-22 16:12:19 +0000

    Lets continue

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,6 @@
+
+// This file is now deprecated and its contents have been moved to lib/demo/demo-data.ts
+// to make its purpose clearer.
 // Direct export of default assets to prevent broken links.
 // These URLs point to publicly accessible files in a Google Cloud Storage bucket.
 export const defaultLogo = 'https://minoritycannabis.org/wp-content/uploads/2025/03/att.yne0IiSw3BIPejWlvseVXNlEGfA5E9CjF7HO2ecLTGQ.jpeg';
