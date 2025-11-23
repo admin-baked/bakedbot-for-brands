fccee0bc4d2b8398ff4ade03dc8e13c5186d11c3
Martez Knox <martez@bakedbot.ai>
2025-11-23 01:25:50 +0000

    ext.js (14.2.3) is outdated (learn more) Server Error Error: Module not

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from '@/providers';
-import Header from '@/components/header';
-import { Footer } from '@/components/footer';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -23,9 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <div className="flex flex-col min-h-screen">
-            <Header />
             <main className="flex-1">{children}</main>
-            <Footer />
           </div>
         </Providers>
       </body>
