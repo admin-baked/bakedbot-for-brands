23166c7ebc2d3355a70d87d2848c07bbd9effc3d
Martez Knox <martez@bakedbot.ai>
2025-11-23 19:13:27 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

---
+++ src/next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bakedbot.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      }
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
