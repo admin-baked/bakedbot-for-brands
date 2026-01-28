1daf6321ae73e34f37c8983cbb5e922850a73aa6
BakedBot Deployer <deploy@bakedbot.ai>
2026-01-27 20:26:24 -0600

    fix(auth): update firebase client api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@
 export const firebaseConfig = {
   projectId: "studio-567050101-bc6e8",
   appId: "1:1016399212569:web:d9c43842ea4d824e13ba88",
-  apiKey: "AIzaSyASUULiUcdtqVnPrTqZTsxoNiXdFPJ5e7E",
+  apiKey: "AIzaSyABrA374LHAvS22L1ztSdY0917K4BZjaaU",
   authDomain: "studio-567050101-bc6e8.firebaseapp.com", // Reverted to default to fix 404
   storageBucket: "studio-567050101-bc6e8.firebasestorage.app",
   measurementId: "G-B4FT9QTWD1",
